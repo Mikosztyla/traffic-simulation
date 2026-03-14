f1c909168e658be6e9f133f0934df1a3a794ca7b
Magdamini <mpabisz@student.agh.edu.pl>
2026-03-14 19:32:40 +0100

    Add CarGenerator class

--- main2.py
+++ main2.py
@@ -8,6 +8,7 @@ from road import Road
 from random import random
 from constants import *
 from traffic_light import TrafficLight
+from car_generator import CarGenerator
 
 pygame.init()
 
@@ -33,21 +34,23 @@ car_image = pygame.transform.scale(car_image, (40, CAR_LENGTH))
 car_pos = pygame.Vector2(WIDTH // 2 - 20, HEIGHT)
 car_speed = 100
 
-car_rect = car_image.get_rect(topleft=car_pos)
+# car_rect = car_image.get_rect(topleft=car_pos)
 
-cars = []
+# cars = []
 
-spawn_timer = 0
-MAX_SPAWN_INTERVAL = 2
-MIN_SPAWN_INTERVAL = 0.5
+# spawn_timer = 0
+# MAX_SPAWN_INTERVAL = 2
+# MIN_SPAWN_INTERVAL = 0.5
 
-spawn_interval = MIN_SPAWN_INTERVAL
+# spawn_interval = MIN_SPAWN_INTERVAL
 
 # road = Road(pygame.Vector2(WIDTH // 2, -CAR_LENGTH), pygame.Vector2(WIDTH // 2, HEIGHT + CAR_LENGTH), Side.S, 14, 2)
 road = Road(pygame.Vector2(WIDTH // 2, HEIGHT + CAR_LENGTH), pygame.Vector2(WIDTH // 2, -CAR_LENGTH), Side.N, 14, 2)
 traffic_lights = [
     TrafficLight(road.lanes[0], (WIDTH // 2 + 60, HEIGHT // 2 - 100), (TRAFFIC_LIGHT_WIDTH, TRAFFIC_LIGHT_HEIGHT)),
     TrafficLight(road.lanes[1], (WIDTH // 2 - 100, HEIGHT // 2 - 100), (TRAFFIC_LIGHT_WIDTH, TRAFFIC_LIGHT_HEIGHT))]
+
+car_generator = CarGenerator([road], 0.1)
 
 while running:
     dt = clock.tick(60) / 1000
@@ -58,25 +61,19 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             for traffic_light in traffic_lights:
                 traffic_light.handle_click(event.pos)
+
     fill_background(screen)
-    road.draw(screen)
+    road.draw(screen, car_image)
+
     for traffic_light in traffic_lights:
         traffic_light.update(dt)
     
-    spawn_timer += dt
-    if spawn_timer >= spawn_interval:
-        speed = random() * 10
-        if random() > 0.8: 
-            speed = 14
-        cars.append(road.spawn_new_car(10, speed))
-        spawn_timer = 0
-        spawn_interval = random() * (MAX_SPAWN_INTERVAL - MIN_SPAWN_INTERVAL) + MIN_SPAWN_INTERVAL
-        # spawn_interval = 100
-
+    car_generator.update(dt)
     road.update_cars(dt)
 
-    for car in cars:
-        car.draw(screen, car_image)
+    # for car in cars:
+    #     car.draw(screen, car_image)
+
     for traffic_light in traffic_lights:
         traffic_light.draw(screen)
     pygame.display.flip()
