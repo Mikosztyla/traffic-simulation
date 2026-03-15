1f42ff93ac9062e90e093877688a60b32cce70d6
Magdamini <mpabisz@student.agh.edu.pl>
2026-03-15 20:54:03 +0100

    Change stop distance; StopCar is no longer a child of Car

--- stop_car.py
+++ stop_car.py
@@ -1,12 +1,10 @@
-from car import Car
 from constants import *
 
-class StopCar(Car):
+class StopCar():
 
     def __init__(self, lane, progress, speed, target_in_lane_index):
-        super().__init__(lane, speed, target_in_lane_index)
         self.last_lane_change = 0
-        self.length = CAR_LENGTH
+        self.length = STOP_OFFSET_METERS * PIXELS_PER_METER * 2
         self.progress = progress
         self.speed = 0
         self.position = lane.start.lerp(lane.end, progress)
